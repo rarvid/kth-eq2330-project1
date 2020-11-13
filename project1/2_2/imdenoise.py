37dd1de35077c420a1d68615684beec464a2b47c
Rudolfs Arvids Kalnins <rudolfsarvidskalnins@gmail.com>
2020-11-13 18:04:28 +0100

    lena AAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ project1/2_2/imdenoise.py
@@ -0,0 +1,9 @@
+# Project 1 
+# 2 Spatial domain processing
+# 2.2 Image denoising
+import numpy as np        # math stuff
+import cv2          # image processing stuff
+from matplotlib import pyplot as plt   # plotting
+from PIL import Image
+
+X = cv2.imread('../lena.512.bmp')

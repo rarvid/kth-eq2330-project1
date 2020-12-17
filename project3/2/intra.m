414644cb9e3ac97afefebd238a94e716e744f39f
DarkKapla <DarkKapla@users.noreply.github.com>
2020-12-17 19:21:59 +0100

    Fix part 2 code
    The bit rate calculation was not correct. Now it is. The best
    kind of correct you'll never wreck.

--- project3/2/intra.m
+++ project3/2/intra.m
@@ -1,109 +1,225 @@
+%-----------------%
+% Header comments %
+%-----------------%
+
 % extract first 50 frames from foreman video
 % cell array with 50 frames(cells) - 176x144 double array
-foreman = yuv_import_y('mother-daughter_qcif.yuv',[176 144],50);
+foreman = yuv_import_y('foreman_qcif.yuv',[176 144],50);
 
-% cell array with 50 frames(cells), each frame contains 18x22 cell array,
-% each cell array cotains 8x8 double arrays - DCT block. DCT2 has been
-% applied to each DCT Block individually
-dct8 = cellfun(@dct_blocks,foreman,'UniformOutput',0);
+data_8_8 = cell(4, 1);
+for i = 3:6
+    % load the DCTed blocks in the data
+    % several steps are short cut in this line
+    vid = cellfun(@dct_blocks, foreman,'UniformOutput',0);
+    % define our step size
+    step_size = 2.^i;
+    % with our step size, we can quantize the DCTed blocks
+    vid = quant_frames(vid, step_size);
+    % store the vidéo in the data
+    data_8_8{i-2, 1} = vid;
+end
 
-% cell array with 4 videos quantized with differnet quantization levels 
-% each with 50 frames where each frames has a 18x22 cell array with 8x8
-% double arrays - Quantized DCT block
-var_quant = quant_mult(dct8);
+% from 8×8 to 16×16
+data = merge_cells(data_8_8);
 
-% cell array with 4 videos each with 50 frames, each frames is a 176x144
-% double array that has been obtained by reconstructing the 8x8 DCT Blocks
-% using IDCT2
-recon = reconstruct(var_quant);
+% Calculate the bit rate with the intra-mode only data
+% so that we have the R1 used in the langragian function.
+rates = cellfun(@bit_rate, data, 'UniformOutput',0);
 
-% cell array with 4 videos, each with 50 frames, where each frame is a
-% 176x144 uint8 array
-rounded_rec = doubleround(recon);
+% convert the rates (bit/pixel) into raw kbit/s (not per pixel any more)
+[w, h] = size(foreman{1,1});
+for i = 1:length(rates)
+    rates{i,1} = rates{i,1}(1) * ( w * h * length(foreman) * 30 / 1000);
+end
 
-% A matlab video format structure to view frames as videos
-vid = cell2video(rounded_rec{4,1});
+% rebuild the original vidéos
+recons = cellfun(@reconstruct_16, data, 'UniformOutput',0);
 
-% Cell array with 4 videos each with 50 PSNR values, i.e. each frame of the
-% reconstructions compared with the original 50 frames
-psnr_frames = psnr_mult(rounded_rec, foreman);
-
-% cell array with average PSNR of 50 frames from 4 different quantization
-% levels
-avg_psnr = avg(psnr_frames);
-
-% obtain 16x16 blocks with 4 8x8 DCT blocks 
-quanted_16_16 = merge_cells(var_quant);
-
-% entropies of each 16x16 block
-entropy_255 = entropy_blocks(quanted_16_16);
-
-% average entropy of each frame
-average_entropy = avg_entropy(entropy_255);
-
-% bitrate calculation
-kbitrate_s = bitrate(average_entropy);
-
+psnrs = psnr_mult(recons, foreman);
 %  PSNR kBit/s plot
-figure('visible','off');
-plot(cell2mat(kbitrate_s)', cell2mat(avg_psnr)','LineWidth',1);
-title('mother-daughter')
+figure('visible','on');
+plot(cell2mat(rates)', cell2mat(psnrs)','LineWidth',1);
+title('')
 xlabel('kbit/s')
 ylabel('PSNR')
 p_b_plot = gca;
-exportgraphics(p_b_plot, 'PSNR_kBit_s_mother_daughter_plot.png');
-
-function b = bitrate(entr)
-  [levels, ~] = size(entr);
-  for i = 1:levels
-    [frames, ~] = size(entr{i,1});
-    duration = frames ./ 30;
-    entropy_matrix = cell2mat(entr{i,1});
-    cum_entropy = sum(entropy_matrix);
-    b{i,1} = cum_entropy ./ duration;
-    b{i,1} = b{i,1} ./ 1000; 
-  end
-end
+exportgraphics(p_b_plot, '2_foreman_plot.png');
 
 
-% Calculate the average entropy of each frame
-function avge = avg_entropy(cellar)
-  [levels, ~] = size(cellar);
-  for i = 1:levels
-    [frames, ~] = size(cellar{i,1});
-    for j = 1:frames
-      matrix = cell2mat(cellar{i,1}{j,1});
-      avge{i,1}{j,1} = mean(matrix(:)); 
-    end
-  end
-end
+%%%%%%%%%%%%%%%% LOCAL FUNCTIONS %%%%%%%%%%%%%%%%%
 
-% get entropies of each 16x16 block
-function e = entropy_blocks(cellar)
-  [levels, ~] = size(cellar);
-  for i = 1:levels
-    [frames, ~] = size(cellar{i,1});
-    for j = 1:frames
-      [blocks_x, blocks_y] = size(cellar{i,1}{j,1});
-      for k = 1:blocks_x
-        for l = 1:blocks_y
-          e{i,1}{j,1}{k,l} = entropy_single(cellar{i,1}{j,1}{k,l});
+% reconstrictopn
+function recons = reconstruct_16(vid)
+    recons = cell(size(vid));
+    
+    deux = size(vid{1,1}) .* size(vid{1,1}{1,1});
+    Lx = deux(1);
+    Ly = deux(2);
+    [Bx, By] = size (vid{1,1});
+    
+    for f = 1:length(vid)
+        recons{f,1} = zeros(Lx, Ly);
+        for i = 1:Bx
+            for j = 1:By
+                recon_i = 16 * (i-1) + 1;
+                recon_j = 16 * (j-1) + 1;
+                recons{f,1}(recon_i:recon_i+15, recon_j:recon_j+15) = idct16 (vid{f,1}{i,j});
+            end
         end
-      end
     end
+
+end
+
+% returns a cell arrary with 'i' cells with 'j' PSNR values, comparing 'i'
+% differnet quantization levels and the original image
+function p = psnr_mult(vid, orig)
+  [levels, ~] = size(vid);
+  for i = 1:levels
+    [frames, ~] = size(vid{i,1});
+    for j = 1:frames
+      psnr_frames{i,1}{j,1} = psnr(vid{i,1}{j,1}, orig{j,1}, 255);
+      p = psnr_frames;
+    end
+  end
+  p = avg(p);
+end
+% Calculate average of cells
+function a = avg(cellar)
+  [levels, ~] = size(cellar);
+  for i = 1:levels
+    a{i,1} = mean(cell2mat(cellar{i,1}));
   end
 end
 
-function en = entropy_single(array)
-  U = unique(array);
-  H = histcounts(array, U);
-  pdf = H / 256;
-  total_entropy = 0;
-  for i = 1:length(pdf)
-    total_entropy = total_entropy + (pdf(i) .* -log2(pdf(i)));
-  end
-  en = total_entropy;
+% Une fonction des plus terrifiantes.
+function rate = bit_rate(vid)
+    [len, ~] = size(vid);
+    [Lx, Ly] = size(vid{1,1});
+    
+    total_pixel_count = len * Lx * Ly;
+    % This array will store the pixels' values before we compute the
+    % entropy on it.
+    pixels = ones(total_pixel_count, 1);
+    % We want a 1-dim list
+    pixels = pixels (:);
+    
+    total_entropy = 0.0;
+    
+    for u = 1:16
+        for v = 1:16
+            % Compute the entropy of all the (u,v)-th coefficients, amoung
+            % all blocks from all frames.
+            
+            % index in the pixels list.
+            idx = 1;
+            
+            for k = 1:len
+                for i = 1:Lx
+                    for j = 1:Ly
+                        
+                        pixel = vid{k,1}{i,j}(u,v);
+                        pixels(idx) = pixel;
+                        idx = idx + 1;
+                        
+                    end
+                end
+            end
+            
+            % Now we can compute the (u,v)-th entropy.
+            list = pixels( 1:(idx-1) );
+            total_entropy = total_entropy + entropy(list);
+        end
+    end
+    
+    rate = total_entropy / 256.0;
+    
 end
+
+function H = entropy(list)
+    uni = unique (list);
+    occurrences = zeros(size(uni));
+    for i = 1:length(list)
+        v = list(i);
+        index = -1;
+        for k = 1:length(uni)
+            if uni(k) == v
+                index = k;
+                break;
+            end
+        end
+        occurrences(index) = occurrences(index) + 1;
+    end
+    pdf = occurrences ./ length(list);
+    H = 0.0;
+    for i = 1:length(pdf)
+        H = H - pdf(i) * log2(pdf(i));
+    end
+    
+end
+
+function new_vid = encode_video(vid, orig_vid, step_size, intra_bit_rate)
+    [len, ~] = size(vid);
+    new_vid = cell(len, 1);
+    new_vid{1,1} = vid{1,1};
+    
+    for i = 2:len
+        new_vid{i,1} = encode_frame(new_vid{i-1, 1},vid{i,1},orig_vid{i,1},0.2*step_size*step_size,intra_bit_rate);
+    end
+end
+
+% Une fonction des plus ténèbreuses.
+function new_frame = encode_frame(previous, current, curr_original, lambda, R1)
+    % previous and current are two consecutive frames (in DCTed
+    % form, encoded in mode intra). Cells filled with 16×16 blocks
+    
+    
+    [Lx, Ly] = size(current);
+    new_frame = cell(Lx, Ly);
+    
+    for i = 1:Lx
+        for j = 1:Ly
+            curr_block = current{i,j}{2,1};
+            orig_i = 16 * (i-1) + 1;
+            orig_j = 16 * (j-1) + 1;
+            orig_block = curr_original(orig_i:orig_i+15, orig_j:orig_j+15);
+            
+            % distortion of mode intra
+            D1 = immse( idct16(curr_block), orig_block);
+            % the bit rate of mode intra, R1, is already given in argument
+            
+            % distortion of mode copy
+            prev_block = previous{i,j}{2,1};
+            D2 = immse( idct16(prev_block), orig_block);
+            % bit rate of mode copy
+            R2 = 1 ./ 256; % yep! only one bit of information of 256 pixels!
+            
+            % the aiguillage is here
+            cellule = cell(2,1);
+            if D1 + lambda * R1 < D2 + lambda * R2
+                % intra mode wins
+                % we do nothing as the current frame is already in intra
+                % mode. The only thing to do it to copy.
+                cellule{1,1} = 1;
+                cellule{2,1} = curr_block;
+            else
+                % copy mode wins
+                cellule{1,1} = 2;
+                cellule{2,1} = prev_block; % normally that cell should be empty
+            end
+            new_frame{i,j} = cellule;
+        end
+    end
+    
+end
+
+function blk = idct16(array)
+    % apply the idct to the four 8×8 sub-blocks
+    blk(1:8, 1:8)   = idct2(array(1:8, 1:8));
+    blk(9:16, 1:8)  = idct2(array(9:16, 1:8));
+    blk(1:8, 9:16)  = idct2(array(1:8, 9:16));
+    blk(9:16, 9:16) = idct2(array(9:16, 9:16));
+end
+
 
 % merge 4 8x8 cell blocks to obatin 16x16 blocks( needed for entropy calc)
 function m = merge_cells(cellar)
@@ -121,37 +237,34 @@ function m = merge_cells(cellar)
   end
 end
 
-
-% Calculate average of cells
-function a = avg(cellar)
-  [levels, ~] = size(cellar);
-  for i = 1:levels
-    a{i,1} = mean(cell2mat(cellar{i,1}));
-  end
-end
-
-% returns a cell arrary with 'i' cells with 'j' PSNR values, comparing 'i'
-% differnet quantization levels and the original image
-function p = psnr_mult(vid, orig)
-  [levels, ~] = size(vid);
-  for i = 1:levels
-    [frames, ~] = size(vid{i,1});
-    for j = 1:frames
-      psnr_frames{i,1}{j,1} = psnr(vid{i,1}{j,1}, orig{j,1}, 255);
-      p = psnr_frames;
-    end
-  end
-end
-
-% Takes a cell with 'i' frames and converts it into a matlab video structure
-function vid = cell2video(cellar)
-  % get number of frames(cells) in cell array
-  [frames, ~] = size(cellar);
+% applies quantizer to video with 'i' frames that have been split into
+% 8x8 DCT blocks
+function q = quant_frames(vid,step)
+  % get number of frames
+  [frames, ~] = size(vid);
+  % apply to all frames
   for i = 1:frames
-    % write individual frames into video structure
-    vid(i).cdata = cellar{i,1};
-    vid(i).colormap = [];
+    % number of blocks in frame in x and y coord
+    [sizex , sizey] = size(vid{i,1}); 
+    % create step size for each block (this is a cellfun thing)
+    step_size = num2cell(step * ones(sizex, sizey)); 
+    % apply quantizer to each 8x8 block in each frame to all frames
+    q{i,1} = cellfun(@quantizer, vid{i,1}, step_size, 'UniformOutput', 0); 
   end
+  
+end
+
+% takes  cell array that contains frames, splits each frame into 8x8 Blocks
+% and applies DCT2 to each block
+function res = dct_blocks(im)
+  % get dimensions of frame
+  [sizex, sizey] = size(im);
+  % calculate number of 16x16 blocks needed
+  xvector_8 = 8*ones(1, sizex ./ 8 );
+  yvector_8 = 8*ones(1, sizey ./ 8 );
+  % split frame into 8x8 blocks
+  blocks_8 = mat2cell(im,xvector_8,yvector_8);
+  res = cellfun(@dct2, blocks_8, 'UniformOutput', 0);
 end
 
 % Takes a cell array with 'i' videos each with 'j' frames and converts the
@@ -181,45 +294,4 @@ function rec = reconstruct(coefs)
     end
   end
 
-end
-
-% applies quantization with step size 2^3..6 on the same given video and
-% returns a cell with videos that have the quantization applied to their
-% DCT 8x8 blocks in each frame
-function mult = quant_mult(vid)
-  % apply quantization to all frames using varying stepsizes
-  for i = 3:6
-    step_size = 2.^i;
-    mult{i-2,1} = quant_frames(vid, step_size);
-  end
-end
-
-% applies quantizer to video with 'i' frames that have been split into
-% 8x8 DCT blocks  
-function q = quant_frames(vid,step)
-  % get number of frames
-  [frames, ~] = size(vid);
-  % apply to all frames
-  for i = 1:frames
-    % number of blocks in frame in x and y coord
-    [sizex , sizey] = size(vid{i,1}); 
-    % create step size for each block (this is a cellfun thing)
-    step_size = num2cell(step * ones(sizex, sizey)); 
-    % apply quantizer to each 8x8 block in each frame to all frames
-    q{i,1} = cellfun(@quantizer, vid{i,1}, step_size, 'UniformOutput', 0); 
-  end
-  
-end
-
-% takes  cell array that contains frames, splits each frame into 8x8 Blocks
-% and applies DCT2 to each block
-function res = dct_blocks(im)
-  % get dimensions of frame
-  [sizex, sizey] = size(im);
-  % calculate number of 16x16 blocks needed
-  xvector_8 = 8*ones(1, sizex ./ 8 );
-  yvector_8 = 8*ones(1, sizey ./ 8 );
-  % split frame into 16x16 blocks
-  blocks_8 = mat2cell(im,xvector_8,yvector_8);
-  res = cellfun(@dct2, blocks_8, 'UniformOutput', 0);
 end
